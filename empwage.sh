df65dbffce482c9a4a8c4c4b3bb90ebf7831b1fd
Ankita <Ankitaangane20@gmail.com>
2020-10-16 16:28:51 +0530

    Added Part time employee wage

--- empwage.sh
+++ empwage.sh
@@ -9,20 +9,31 @@ echo "WELCOME to Employee Wage Computaion Program!"
 
 #UC1:Check for attendance
 #UC2:Calculating Daily Employee Wage
+#UC3:Added part time employee wage
 
+#Variables declaration
 empAbsent=0
+empRatePerHr=20
+partTime=1
+fullTime=2
+empHr=0
+
 ranCheck=$((RANDOM%3))
 
 if [ $empAbsent -eq $ranCheck ]
 then
         echo "Employee is absent"
-	salary=0
+	empHr=0
 else
         echo "Employee is present"
-	empRatePerHr=20
-	empHr=8
-	salary=$(( empHr * empRatePerHr ))
+	if [ $fullTime -eq $ranCheck ]
+	then
+		empHr=8
+	else
+		empHr=4
+	fi
 fi
 
+salary=$(( empHr * empRatePerHr ))
 echo "Salary of employee is $salary"
 
