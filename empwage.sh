b954fe362d3c805b96e8a55634ddd703242fb6c8
Ankita <Ankitaangane20@gmail.com>
2020-10-16 16:18:10 +0530

    Added daily salary of employee

--- empwage.sh
+++ empwage.sh
@@ -8,12 +8,21 @@ echo "WELCOME to Employee Wage Computaion Program!"
 
 
 #UC1:Check for attendance
+#UC2:Calculating Daily Employee Wage
+
 empAbsent=0
 ranCheck=$((RANDOM%3))
 
 if [ $empAbsent -eq $ranCheck ]
 then
         echo "Employee is absent"
+	salary=0
 else
         echo "Employee is present"
+	empRatePerHr=20
+	empHr=8
+	salary=$(( empHr * empRatePerHr ))
 fi
+
+echo "Salary of employee is $salary"
+
