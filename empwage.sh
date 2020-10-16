e3e960ef5bac1aebbf622f13a7fa3e082bacc07d
Ankita <Ankitaangane20@gmail.com>
2020-10-16 16:09:40 +0530

    Added attendace for employee

--- empwage.sh
+++ empwage.sh
@@ -5,3 +5,15 @@
 
 #Start of program
 echo "WELCOME to Employee Wage Computaion Program!"
+
+
+#UC1:Check for attendance
+empAbsent=0
+ranCheck=$((RANDOM%3))
+
+if [ $empAbsent -eq $ranCheck ]
+then
+        echo "Employee is absent"
+else
+        echo "Employee is present"
+fi
